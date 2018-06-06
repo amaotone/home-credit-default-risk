c45caf0c74dd8e62940580a6a50eae2d710b59a4
Amane Suzuki <amane.suzu@gmail.com>
2018-06-06 11:18:45 +0900

    Add convert_to_feather

--- scripts/convert_to_feather.py
+++ scripts/convert_to_feather.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
+from pathlib import Path
+
+for f in Path('../input').glob('*.csv.gz'):
+    out = f.parent / f.name.replace('.csv.gz', '.ftr')
+    pd.read_csv(f, index_col=0).to_feather(out)
+
 pd.read_feather('../input/application_train.ftr')[['SK_ID_CURR']].to_feather('../input/train_idx.ftr')
 pd.read_feather('../input/application_test.ftr')[['SK_ID_CURR']].to_feather('../input/test_idx.ftr')
